5c67487ef23fdf0ed4178f04d5e476c417354b36
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-03 20:48:05 +0600

    footer section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
             <Educations />
             <Projects />
             <Contacts />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 };
